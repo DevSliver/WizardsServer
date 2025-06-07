08a1a8e42f2893c276bf4d9f1b52ae78ce7a6c3d
SLiverRUS <mamchenkonikita371@gmail.com>
2025-06-07 04:35:37 +0300

    1

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace WizardsServer
             var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
             var server = new Server(IPAddress.Any, Convert.ToInt32(port));
             server.Start();
-            Console.WriteLine($"Сервер запущен на порту {port}. Нажмите Ctrl+C для остановки.");
+            Console.WriteLine($"Сервер запущен на порту {port}. Нажмите Ctrl+C для остановки...");
 
             // Ожидаем завершения работы сервера
             await Task.Delay(-1);
