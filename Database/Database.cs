7a31af83b2c784107426261a4d8b52c8bccf6098
SLiverRUS <mamchenkonikita371@gmail.com>
2025-06-09 23:31:13 +0300

    Изменил поведение класса базы данных.

--- Database/Database.cs
+++ Database/Database.cs
@@ -1,25 +1,32 @@
-﻿using System;
-using System.Security.Cryptography;
-using System.Text;
+﻿namespace WizardsServer;
+
 using Npgsql;
+using System;
 
-namespace WizardsServer
+public static class Database
 {
-    public static class Database
+    private static string _connectionString;
+
+    public static void Initialize(string connectionString)
     {
-        private static NpgsqlConnection _connection;
+        _connectionString = connectionString;
 
-        public static void Initialize(string connectionString)
-        {
-            _connection = new NpgsqlConnection(connectionString);
-            _connection.Open();
-            Console.WriteLine("Подключение к базе данных установлено");
-        }
-
-        public static NpgsqlCommand CreateCommand(string sql)
-        {
-            return new NpgsqlCommand(sql, _connection);
-        }
+        // Проверочное подключение, чтобы убедиться, что строка валидна
+        using var connection = new NpgsqlConnection(_connectionString);
+        connection.Open();
+        Console.WriteLine("Подключение к базе данных установлено");
     }
 
+    public static NpgsqlCommand CreateCommand(string sql)
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        connection.Open();
+
+        var command = new NpgsqlCommand(sql, connection);
+
+        // Чтобы соединение не пропало, привязываем его к команде и закрываем позже вручную
+        return command;
+    }
 }
+
+
